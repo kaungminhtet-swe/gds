8d82c7bfc58cbb2bcbc1333280bf75890bac88c7
kgminhtet <kgminhtet.dev@gmail.com>
2025-01-15 08:56:40 +0630

    Add list interface

---
+++ list/list.go
@@ -0,0 +1,27 @@
+package list
+
+import (
+	"github.com/mg-kaung/gds/iterator"
+)
+
+type List[T comparable] interface {
+	iterator.Iterator[T]
+
+	InsertAll(value ...T)
+	InsertList(l *List[T])
+
+	InsertFront(value T)
+	InsertBack(value T)
+	InsertIndex(index int, value T)
+
+	RemoveFront()
+	RemoveBack()
+	RemoveIndex(index int)
+
+	Front() T
+	Back() T
+	GetIndex(index int) T
+
+	Len() int
+	Init()
+}
